9db63912d3796a656cbc31dcab14c6d844429e6d
Abdulkadir Adiguzel <abdulkadiradiguzelaa@outlook.com>
2024-09-18 12:16:37 +0200

    FINAL

--- user/script.js
+++ user/script.js
@@ -1,12 +1,19 @@
+function updateCartCount() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+    const cartCountElement = document.getElementById('cart-count');
+
+    if (cartCount > 0) {
+        cartCountElement.textContent = cartCount;
+        cartCountElement.style.display = 'block';
+    } else {
+        cartCountElement.style.display = 'none';
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const products = JSON.parse(localStorage.getItem('products')) || [];
-    console.log('Products loaded from localStorage:', products);
-
     const productsContainer = document.getElementById("midden");
-    if (!productsContainer) {
-        console.error('Element with id "midden" not found.');
-        return;
-    }
 
     productsContainer.innerHTML = '';
 
@@ -14,8 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
         .map((product, originalIndex) => ({ ...product, originalIndex }))
         .filter(product => product.quantity > 0)
         .slice(0, 6);
-
-    console.log('Displayed Products:', displayedProducts);
 
     displayedProducts.forEach((product) => {
         const productElement = document.createElement("a");
